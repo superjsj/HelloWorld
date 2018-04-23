6b93dde1c8aebc2c374275289e1c501d8551d57f
Ji, Colin <shaojun.ji@kodak.com>
2018-04-23 13:47:34 +0800

    Change word

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World1");
+            Console.WriteLine("Hello World21");
         }
     }
 }
