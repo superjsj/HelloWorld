6f62e5a8b49d70991c6dacfd554b44fc7d1dc207
Ji, Colin <shaojun.ji@kodak.com>
2018-04-23 13:51:17 +0800

    revert

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World21");
+            Console.WriteLine("Hello World");
         }
     }
 }
